5e47e8692f109a171e8b3b7c80edff28b9f330ab
yuryalencar <yuryalencar@yuryalencar>
2016-10-04 03:37:00 -0300

    @yuryalencar: Commit inicial com todos os pacotes e classes criadas, mas claro que terá novas classes.
    Próxima etapa: Juntar e ver como vamos implementar tudo.

---
+++ SportManager/src/org/unipampa/sportmanager/aplicacao/Aplicacao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unipampa.sportmanager.aplicacao;
+
+import org.unipampa.sportmanager.frames.Inicial;
+/**
+ *
+ * @author yuryalencar
+ */
+public class Aplicacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Inicial().setVisible(true);
+    }
+    
+}
